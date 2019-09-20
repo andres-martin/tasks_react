72b3cd9baaa31846c9243297856aeb4d20e6c6cd
andres-martin <andresmartin266@gmail.com>
2019-09-19 20:07:39 -0500

    [feature] add a new item to the state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newItem: '',
+      items: []
+    };
+    // when using new ES& rrow functions there's no need to bind each function
+    // to the constructor
+    // this.addItem = this.addItem.bind(this);
+    // this.submit = this.submit.bind(this);
+  }
+
   render() {
     return (
       <div className="wrapper">
@@ -12,14 +24,37 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.items.map(text => <li key={text}>{text}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+
+          <form onSubmit={this.submitItem} >
+            <input type="text" onChange={this.addItem} value={this.state.newItem}
+              id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+          </form>
         </div>
-      </div>
+      </div >
     )
   }
+  addItem = e => {
+    this.setState({ newItem: e.target.value });
+  };
+
+  submitItem = (e) => {
+    e.preventDefault();
+    if (this.state.newItem !== '') {
+      this.setState(state => {
+        const items = [...state.items, state.newItem];
+        return {
+          items,
+          newItem: ''
+        }
+      });
+    }
+
+  }
+
+
+
 }
 
 
